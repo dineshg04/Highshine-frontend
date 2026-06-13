d19e06591a5f26d460995fee12961d092fbf536d
dineshg04 <dineshkumarg2206@gmail.com>
2026-06-13 22:07:38 +0530

    googel analytics id

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-0PHB3STGQB");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
